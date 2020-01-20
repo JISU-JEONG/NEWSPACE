b38e2d1769704e354699ed234ed1c89b24c11c1a
정영길 <younggil94@naver.com>
2020-01-20 17:58:18 +0900

    firebase login + google

--- front/src/components/Navigation.vue
+++ front/src/components/Navigation.vue
@@ -1,23 +1,17 @@
 <template>
-    <v-bottom-navigation color="deep-purple accent-4">
-    
-        <v-btn to="/">
-    
-            <span>Home</span>
-    
-        </v-btn>
-    
-        <v-btn to="/portfolio">
-    
-            <span>Portfolio</span>
-    
-        </v-btn>
-    
-        <v-btn to="/post">
-    
-            <span>Post</span>
-    
-        </v-btn>
-    
-    </v-bottom-navigation>
+  <v-bottom-navigation color="deep-purple accent-4">
+    <v-btn to="/">
+      <span>Home</span>
+    </v-btn>
+
+    <v-btn to="/portfolio">
+      <span>Portfolio</span>
+    </v-btn>
+
+    <v-btn to="/post">
+      <span>Post</span>
+    </v-btn>
+
+    <v-btn to="/Login">Login</v-btn>
+  </v-bottom-navigation>
 </template>
