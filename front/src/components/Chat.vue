86626e9fd6029193d47e3af4754582c437412f94
EHwooKim <ehwoo0707@naver.com>
2020-02-11 10:35:26 +0900

    로그인/로그아웃 상태 채팅창 수정중

--- front/src/components/Chat.vue
+++ front/src/components/Chat.vue
@@ -39,6 +39,8 @@
             ref="form"
             solo
             v-model="message"
+            :disabled="disabled"
+            :label="label"
             type="text"
             append-outer-icon="mdi-send"
             @click:append-outer="sendMessage"
@@ -68,12 +70,15 @@ export default {
   data() {
     return {
       show: false,
-      countReadMessages: 0,
+      startChat: false,  // 채팅연결 상태, true: 연결된 상태
+      disabled: true, // 로그인 안한 사용자 채팅 막기
+      label: "로그인이 필요합니다.",
+      countReadMessages: 0, // 읽지않은 메세지 카운트
       message: "",
       usernumber: 0,
       userlist:[],
       receivemessage: [],
-      fromMe: 'from-me', // 동적 css 위한 변수저장
+      fromMe: 'from-me', // css class 바인딩 위한 변수
       fromThem: 'from-them',
       autofocus: false, // 채팅창 켜지면 입력창에 바로 focus되게 하려 했으나..
     };
@@ -81,14 +86,22 @@ export default {
   computed: {
     countUnreadMessages: function() {
       return this.receivemessage.length - this.countReadMessages
+    },
+    member_name: function() {
+      return this.$store.state.member_name
     }
   },
   methods: {
-    openChat() { // 채팅창 열고 닫기
-      this.show = !this.show
+    openChat() { // 채팅창 버튼 클릭시
+      this.show = !this.show // 채팅창 열고 닫기
       if (!this.show) {
-        this.countReadMessages = this.receivemessage.length
+        this.countReadMessages = this.receivemessage.length  // 안읽은 메세제 계산을위한 변수
       } 
+      if (this.member_name && !this.startChat) { // 로그인했고, 아직 채팅이 연결 안된 상태라면
+        this.connect();   
+        this.startChat = true 
+      }
+
       this.autofocus = !this.autofocus
     },
     connect() {
@@ -133,19 +146,9 @@ export default {
     onMessageReceived(payload) {
       var message = JSON.parse(payload.body);
       if (message.type === "JOIN") {
-        if(message.sessionid===localStorage.getItem("member_id")){
-          this.receivemessage.push({from_me:message.sessionid, content:message.sender + "님이 들어오셨습니다.", sender:"system"});
-        }
-        else{
-          this.receivemessage.push({from_me:message.sessionid, content:message.sender + "님이 들어오셨습니다.", sender:"system"});
-        }
-      } else if (message.type === "LEAVE") {
-        if(message.sessionid===localStorage.getItem("member_id")){
-          this.receivemessage.push({from_me:message.sessionid, content:message.sender + "님이 떠나셨습니다.", sender:"system"});
-        }
-        else{
-          this.receivemessage.push({from_me:message.sessionid, content:message.sender + "님이 떠나셨습니다", sender:"system"});
-        }
+        this.receivemessage.push({from_me:message.sessionid, content:message.sender + "님이 들어오셨습니다.", sender:"system"});
+      } else if (message.type === "LEAVE") {  
+        this.receivemessage.push({from_me:message.sessionid, content:message.sender + "님이 떠나셨습니다", sender:"system"});
       } else if (message.type === "JOINUSER") {
         this.userlist = message.users;
         this.usernumber = message.usernumber;
@@ -156,6 +159,17 @@ export default {
         } else{
           this.receivemessage.push({from_me:false, content:message.content, sender:message.sender});
         }
+      }
+    }
+  },
+  watch: {
+    member_name() { // member_name에 따라 채팅창 이용가능/불가능
+      if (this.member_name) {
+        this.disabled = false
+        this.label = ''
+      } else {
+        this.disabled = true
+        this.label = '로그인이 필요합니다.'
       }
     }
   },
@@ -180,9 +194,9 @@ export default {
     console.log(nick[randomnick] + " " + name[randomname]);
     console.log(randomid);
   },
-  mounted() {
-    this.connect();
-  },
+  // mounted() {
+  //   this.connect();
+  // },
 	updated() {
     // 채팅창 스크롤 맨 아래로 내리기
 		this.$nextTick(function () {
@@ -274,30 +288,6 @@ ul {
   border-radius: 15px;
   list-style: none;
 }
-/* .from-me li:before {
-  content: "";
-  position: absolute;
-  z-index: -1;
-  bottom: -2px;
-  right: -7px;
-  height: 20px;
-  border-right: 20px solid #0B93F6;
-  border-bottom-left-radius: 16px 14px;
-  transform: translate(0, -2px);
-} */
-/* .from-me li:after {
-  content: "";
-  position: absolute;
-  z-index: 1;
-  bottom: -2px;
-  right: -56px;
-  width: 26px;
-  height: 20px;
-  background: white;
-  border-bottom-left-radius: 10px;
-  transform: translate(-30px, -2px);
-} */
-
 .from-them {
   display: flex;
   flex-direction: column;
@@ -319,30 +309,12 @@ ul {
   border-radius:  15px;
   color: black;
   list-style: none;
+}
+@media screen and (max-width: 960px) {
+  body {
 
+  } 
 }
 
-/* .from-them li:before {
-  content: "";
-  position: absolute;
-  z-index: 2;
-  bottom: -2px;
-  left: -7px;
-  height: 20px;
-  border-left: 20px solid #fff;
-  border-bottom-right-radius: 16px 14px;
-  transform: translate(0, -2px);
-}
-.from-them li:after {
-  content: "";
-  position: absolute;
-  z-index: 3;
-  bottom: -2px;
-  left: 4px;
-  width: 26px;
-  height: 20px;
-  background: white;
-  border-bottom-right-radius: 10px;
-  transform: translate(-30px, -2px);
-} */
+
 </style>
